9cf418995f1d52924c2c4c709562fa270c711544
Biplap Bhattarai <bhattaraib58@gmail.com>
2018-06-27 12:22:50 +0545

    assignment 2 2no completed

--- Assignment/src/Assignment6/ImplementRunnable.java
+++ Assignment/src/Assignment6/ImplementRunnable.java
@@ -14,31 +14,62 @@ Second child class should print all the prime number from 300 to 400
  * @author Admin
  */
 
-
 class Thread6 implements Runnable 
 {
     Thread t1;
     Thread6(String name)
     {
         t1=new Thread(this);
-        t1.setName("Biplap");
+        t1.setName(name);
         t1.start();
     }
     @Override
     public void run()
     {  
-        for (int i = 1; i < 100; i++)
+        for (int i=2; i<100; i++)
         {
-            System.out.println(t1.getName());
+            for (int j=2; j<=i; j++)
+            {
+                if (i == j)
+                    System.out.println("Prime numbers in ::"+t1.getName()+" is ::"+i);
+                else if (i%j == 0)
+                    break;
+            }
         }
                 
     }
 }
 
+class Thread7 implements Runnable 
+{
+    Thread t2;
+    Thread7(String name)
+    {
+        t2=new Thread(this);
+        t2.setName(name);
+        t2.start();
+    }
+    @Override
+    public void run()
+    {  
+        for (int i=300; i<400; i++)
+        {
+            for (int j=300; j<=i; j++)
+            {
+                if (i == j)
+                    System.out.println("Prime numbers in ::"+t2.getName()+" is ::"+i);
+                else if (i%j == 0)
+                    break;
+            }
+        }
+                
+    }
+}
 public class ImplementRunnable
 {
     public static void main(String[] args)
     {
-        
+        Thread6 t6=new Thread6("Thread1");
+        Thread7 t7=new Thread7("Thread2");
     }
 }
